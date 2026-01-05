225af784946febd9f45dbf563f6d33a61d067988
mahmoud0ssama <mahmoud02505@gmail.com>
2026-01-05 09:13:44 +0200

    Feat: implementation of Actor classes

---
+++ include/Admin.h
@@ -0,0 +1,28 @@
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+
+#include "Person.h"
+
+class Librarian : public Person {
+public:
+    Librarian(int id, string n, string e, string p)
+        : Person(id, n, e, p, "Librarian") {}
+
+    void addBook() {
+        cout << "Book added\n";
+    }
+
+    void editBook() {
+        cout << "Book edited\n";
+    }
+
+    void deleteBook() {
+        cout << "Book deleted\n";
+    }
+
+    void generateReports() {
+        cout << "Reports generated\n";
+    }
+};
+
+#endif
---
+++ include/Librarian.h
@@ -0,0 +1,16 @@
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+
+
+class Librarian
+{
+    public:
+        Librarian();
+        virtual ~Librarian();
+
+    protected:
+
+    private:
+};
+
+#endif // LIBRARIAN_H
